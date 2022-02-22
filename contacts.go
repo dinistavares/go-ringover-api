45737b1bf132ad9637f4728b870bc0d82668cbf1
Dinis Tavares <dinis07@users.noreply.github.com>
2022-02-22 08:19:26 +0000

    changed whitesapce to spaces rather than tabs, removed a fmt

--- contacts.go
+++ contacts.go
@@ -8,220 +8,220 @@ import (
 )
 
 type SearchedContacts struct {
-	UserID            int       `json:"user_id"`
-	TeamID            int       `json:"team_id"`
-	LimitOffsetSetted int       `json:"limit_offset_setted"`
-	LimitCountSetted  int       `json:"limit_count_setted"`
-	TotalContactCount int       `json:"total_contact_count"`
-	ContactListCount  int       `json:"contact_list_count"`
-	ContactList       []Contact `json:"contact_list"`
+  UserID            int       `json:"user_id"`
+  TeamID            int       `json:"team_id"`
+  LimitOffsetSetted int       `json:"limit_offset_setted"`
+  LimitCountSetted  int       `json:"limit_count_setted"`
+  TotalContactCount int       `json:"total_contact_count"`
+  ContactListCount  int       `json:"contact_list_count"`
+  ContactList       []Contact `json:"contact_list"`
 }
 
 type Contact struct {
-	ContactID        int         `json:"contact_id"`
-	IsShared         bool        `json:"is_shared"`
-	ImOwner          bool        `json:"im_owner"`
-	SocialService    interface{} `json:"social_service"`
-	SocialServiceID  interface{} `json:"social_service_id"`
-	SocialProfileURL interface{} `json:"social_profile_url"`
-	SocialData       interface{} `json:"social_data"`
-	Firstname        string      `json:"firstname"`
-	Lastname         string      `json:"lastname"`
-	Company          string      `json:"company"`
-	ConcatName       string      `json:"concat_name"`
-	Color            string      `json:"color"`
-	Initial          string      `json:"initial"`
-	ProfilePicture   string      `json:"profile_picture"`
-	CreationDate     time.Time   `json:"creation_date"`
-	Numbers          []Number    `json:"numbers"`
+  ContactID        int         `json:"contact_id"`
+  IsShared         bool        `json:"is_shared"`
+  ImOwner          bool        `json:"im_owner"`
+  SocialService    interface{} `json:"social_service"`
+  SocialServiceID  interface{} `json:"social_service_id"`
+  SocialProfileURL interface{} `json:"social_profile_url"`
+  SocialData       interface{} `json:"social_data"`
+  Firstname        string      `json:"firstname"`
+  Lastname         string      `json:"lastname"`
+  Company          string      `json:"company"`
+  ConcatName       string      `json:"concat_name"`
+  Color            string      `json:"color"`
+  Initial          string      `json:"initial"`
+  ProfilePicture   string      `json:"profile_picture"`
+  CreationDate     time.Time   `json:"creation_date"`
+  Numbers          []Number    `json:"numbers"`
 }
 
 type Number struct {
-	Number int64        `json:"number"`
-	Type   string       `json:"type"`
-	Format NumberFormat `json:"format"`
+  Number int64        `json:"number"`
+  Type   string       `json:"type"`
+  Format NumberFormat `json:"format"`
 }
 
 type NumberFormat struct {
-	Raw              int64  `json:"raw"`
-	CountryCode      string `json:"country_code"`
-	Country          string `json:"country"`
-	E164             string `json:"e164"`
-	International    string `json:"international"`
-	InternationalAlt string `json:"international_alt"`
-	National         string `json:"national"`
-	NationalAlt      string `json:"national_alt"`
-	Rfc3966          string `json:"rfc3966"`
+  Raw              int64  `json:"raw"`
+  CountryCode      string `json:"country_code"`
+  Country          string `json:"country"`
+  E164             string `json:"e164"`
+  International    string `json:"international"`
+  InternationalAlt string `json:"international_alt"`
+  National         string `json:"national"`
+  NationalAlt      string `json:"national_alt"`
+  Rfc3966          string `json:"rfc3966"`
 }
 
 type NewContacts struct {
-	Contacts []NewContact `json:"contacts"`
+  Contacts []NewContact `json:"contacts"`
 }
 
 type NewContact struct {
-	Firstname string       `json:"firstname"`
-	Lastname  string       `json:"lastname"`
-	Company   string       `json:"company"`
-	IsShared  bool         `json:"is_shared"`
-	Numbers   []NewNumber `json:"numbers"`
+  Firstname string       `json:"firstname"`
+  Lastname  string       `json:"lastname"`
+  Company   string       `json:"company"`
+  IsShared  bool         `json:"is_shared"`
+  Numbers   []NewNumber `json:"numbers"`
 }
 
 type NewNumber struct {
-	Number int64  `json:"number"`
-	Type   string `json:"type"`
+  Number int64  `json:"number"`
+  Type   string `json:"type"`
 }
 
 type ContactFilter struct {
-	Pagination        string `json:"pagination"`
-	AlphabeticalOrder string `json:"alphabetical_order"`
-	Search            string `json:"search"`
-	LimitCount        int64  `json:"limit_count"`
-	LimitOffset       int64  `json:"limit_offset"`
+  Pagination        string `json:"pagination"`
+  AlphabeticalOrder string `json:"alphabetical_order"`
+  Search            string `json:"search"`
+  LimitCount        int64  `json:"limit_count"`
+  LimitOffset       int64  `json:"limit_offset"`
 }
 
 type Team struct {
-	TeamID            int         `json:"team_id"`
-	Name              string      `json:"name"`
-	TotalNumbersCount int         `json:"total_numbers_count"`
-	Numbers           []Number    `json:"numbers"`
-	TotalUsersCount   int         `json:"total_users_count"`
-	Users             []User      `json:"users"`
-	TotalIvrsCount    int         `json:"total_ivrs_count"`
-	Ivrs              interface{} `json:"ivrs"`
-	TotalTagsCount    int         `json:"total_tags_count"`
-	Tags              interface{} `json:"tags"`
-	TotalGroupsCount  int         `json:"total_groups_count"`
-	Groups            []Groups    `json:"groups"`
+  TeamID            int         `json:"team_id"`
+  Name              string      `json:"name"`
+  TotalNumbersCount int         `json:"total_numbers_count"`
+  Numbers           []Number    `json:"numbers"`
+  TotalUsersCount   int         `json:"total_users_count"`
+  Users             []User      `json:"users"`
+  TotalIvrsCount    int         `json:"total_ivrs_count"`
+  Ivrs              interface{} `json:"ivrs"`
+  TotalTagsCount    int         `json:"total_tags_count"`
+  Tags              interface{} `json:"tags"`
+  TotalGroupsCount  int         `json:"total_groups_count"`
+  Groups            []Groups    `json:"groups"`
 }
 
 type User struct {
-	UserID     int      `json:"user_id"`
-	TeamID     int      `json:"team_id"`
-	Initial    string   `json:"initial"`
-	Color      string   `json:"color"`
-	Firstname  string   `json:"firstname"`
-	Lastname   string   `json:"lastname"`
-	Company    string   `json:"company"`
-	Email      string   `json:"email"`
-	Picture    string   `json:"picture"`
-	ConcatName string   `json:"concat_name"`
-	Numbers    []Number `json:"numbers,omitempty"`
+  UserID     int      `json:"user_id"`
+  TeamID     int      `json:"team_id"`
+  Initial    string   `json:"initial"`
+  Color      string   `json:"color"`
+  Firstname  string   `json:"firstname"`
+  Lastname   string   `json:"lastname"`
+  Company    string   `json:"company"`
+  Email      string   `json:"email"`
+  Picture    string   `json:"picture"`
+  ConcatName string   `json:"concat_name"`
+  Numbers    []Number `json:"numbers,omitempty"`
 }
 
 type Groups struct {
-	GroupID         int         `json:"group_id"`
-	Name            string      `json:"name"`
-	TotalUsersCount int         `json:"total_users_count"`
-	Color           interface{} `json:"color"`
-	IsJumper        bool        `json:"is_jumper"`
+  GroupID         int         `json:"group_id"`
+  Name            string      `json:"name"`
+  TotalUsersCount int         `json:"total_users_count"`
+  Color           interface{} `json:"color"`
+  IsJumper        bool        `json:"is_jumper"`
 }
 
 // List Contacts by Filter
 func (client *Client) ListContactsByFilter(contactfilter ContactFilter) (*[]Contact, error) {
 
-	req, _ := client.NewRequest("POST", "/contacts", contactfilter)
+  req, _ := client.NewRequest("POST", "/contacts", contactfilter)
 
-	searchedContacts := SearchedContacts{}
+  searchedContacts := SearchedContacts{}
 
-	data, _, err := client.Do(req)
+  data, _, err := client.Do(req)
 
-	if err != nil {
-		return nil, err
-	}
+  if err != nil {
+    return nil, err
+  }
 
-	json.Unmarshal(data, &searchedContacts)
+  json.Unmarshal(data, &searchedContacts)
 
-	if searchedContacts.ContactList == nil {
-		return nil, errors.New("no contacts found")
-	}
+  if searchedContacts.ContactList == nil {
+    return nil, errors.New("no contacts found")
+  }
 
-	return &searchedContacts.ContactList, nil
+  return &searchedContacts.ContactList, nil
 }
 
 // Create A New Contact
 func (client *Client) CreateNewContact(newConacts NewContacts) error {
 
-	req, _ := client.NewRequest("POST", "/contacts", newConacts)
+  req, _ := client.NewRequest("POST", "/contacts", newConacts)
 
-	_, _, err := client.Do(req)
+  _, _, err := client.Do(req)
 
-	if err != nil {
-		return err
-	}
+  if err != nil {
+    return err
+  }
 
-	return nil
+  return nil
 }
 
 // Update A Contact
 func (client *Client) UpdateContactByID(contactID string, newContact NewContact) error {
-	url := "/contacts/" + contactID
+  url := "/contacts/" + contactID
 
-	req, _ := client.NewRequest("PUT", url, newContact)
+  req, _ := client.NewRequest("PUT", url, newContact)
 
-	_, _, err := client.Do(req)
+  _, _, err := client.Do(req)
 
-	if err != nil {
-		return err
-	}
+  if err != nil {
+    return err
+  }
 
-	return nil
+  return nil
 }
 
 // Add number to specific contact 
 func (client *Client) AddNewNumberToExistingContact(contactID string, newNumber NewNumber) error {
 
-	numbers := []NewNumber{}
-	numbers = append(numbers, newNumber)
+  numbers := []NewNumber{}
+  numbers = append(numbers, newNumber)
 
-	url := strings.Join([]string{"/contacts", contactID, "numbers"}, "/")
+  url := strings.Join([]string{"/contacts", contactID, "numbers"}, "/")
 
-	req, _ := client.NewRequest("POST", url, numbers)
+  req, _ := client.NewRequest("POST", url, numbers)
 
-	_, _, err := client.Do(req)
+  _, _, err := client.Do(req)
 
-	if err != nil {
-		return err
-	}
+  if err != nil {
+    return err
+  }
 
-	return nil
+  return nil
 }
 
 // Delete A Contact
 func (client *Client) DeleteContactByID(contactID string) error  {
-	
-	url := "/contacts/" + contactID
+  
+  url := "/contacts/" + contactID
 
-	req, _ := client.NewRequest("DELETE", url, nil)
+  req, _ := client.NewRequest("DELETE", url, nil)
 
-	_, _, err := client.Do(req)
+  _, _, err := client.Do(req)
 
-	if err != nil {
-		return err
-	}
+  if err != nil {
+    return err
+  }
 
-	return nil
+  return nil
 
 }
 
 // Get Users in Teams
 func (client *Client) GetUsersInTeams() (*[]User, error) {
 
-	req, _ := client.NewRequest("GET", "/teams", nil)
+  req, _ := client.NewRequest("GET", "/teams", nil)
 
-	team := Team{}
+  team := Team{}
 
-	data, _, err := client.Do(req)
+  data, _, err := client.Do(req)
 
-	if err != nil {
-		return nil, err
-	}
+  if err != nil {
+    return nil, err
+  }
 
-	json.Unmarshal(data, &team)
+  json.Unmarshal(data, &team)
 
-	if team.Users == nil {
-		return nil, errors.New("no users found")
-	}
+  if team.Users == nil {
+    return nil, errors.New("no users found")
+  }
 
-	return &team.Users, nil
+  return &team.Users, nil
 
 }
--- ringover.go
+++ ringover.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -12,110 +11,108 @@ import (
 )
 
 const (
-	defaultRestEndpointURL     = "https://public-api.ringover.com/v2"
-	defaultRestEndpointVersion = "v2"
-	defaultHeaderName          = "Authorization"
-	acceptedContentType        = "application/json"
+  defaultRestEndpointURL     = "https://public-api.ringover.com/v2"
+  defaultRestEndpointVersion = "v2"
+  defaultHeaderName          = "Authorization"
+  acceptedContentType        = "application/json"
 )
 
 var errorDoNilRequest = errors.New("request could not be constructed")
 
 type ClientConfig struct {
-	HttpClient          *http.Client
-	RestEndpointURL     string
-	RestEndpointVersion string
+  HttpClient          *http.Client
+  RestEndpointURL     string
+  RestEndpointVersion string
 }
 
 type auth struct {
-	HeaderName string
-	ApiKey     string
+  HeaderName string
+  ApiKey     string
 }
 
 type Client struct {
-	config *ClientConfig
-	client *http.Client
-	auth   *auth
+  config *ClientConfig
+  client *http.Client
+  auth   *auth
 
-	BaseURL *url.URL
+  BaseURL *url.URL
 }
 
 // New returns a new APi Client
 func New() *Client {
-	config := ClientConfig{}
+  config := ClientConfig{}
 
-	config.HttpClient = http.DefaultClient
-	config.RestEndpointURL = defaultRestEndpointURL
-	config.RestEndpointVersion = defaultRestEndpointVersion
+  config.HttpClient = http.DefaultClient
+  config.RestEndpointURL = defaultRestEndpointURL
+  config.RestEndpointVersion = defaultRestEndpointVersion
 
-	// Create client
-	baseURL, _ := url.Parse(config.RestEndpointURL)
+  // Create client
+  baseURL, _ := url.Parse(config.RestEndpointURL)
 
-	client := &Client{config: &config, client: config.HttpClient, auth: &auth{}, BaseURL: baseURL}
+  client := &Client{config: &config, client: config.HttpClient, auth: &auth{}, BaseURL: baseURL}
 
-	return client
+  return client
 }
 
 // Authenticate saves authenitcation parameters for user
 func (client *Client) Authenticate(apiKey string) {
-	client.auth.HeaderName = defaultHeaderName
-	client.auth.ApiKey = apiKey
+  client.auth.HeaderName = defaultHeaderName
+  client.auth.ApiKey = apiKey
 }
 
 // NewRequest creates an API request
 func (client *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
-	rel, err := url.Parse(client.config.RestEndpointVersion + urlStr)
-	if err != nil {
-		return nil, err
-	}
+  rel, err := url.Parse(client.config.RestEndpointVersion + urlStr)
+  if err != nil {
+    return nil, err
+  }
 
-	url := client.BaseURL.ResolveReference(rel)
+  url := client.BaseURL.ResolveReference(rel)
 
-	fmt.Println(url)
+  var buf io.ReadWriter
+  if body != nil {
+    buf = new(bytes.Buffer)
+    err := json.NewEncoder(buf).Encode(body)
+    if err != nil {
+      return nil, err
+    }
+  }
 
-	var buf io.ReadWriter
-	if body != nil {
-		buf = new(bytes.Buffer)
-		err := json.NewEncoder(buf).Encode(body)
-		if err != nil {
-			return nil, err
-		}
-	}
+  req, err := http.NewRequest(method, url.String(), buf)
+  if err != nil {
+    return nil, err
+  }
 
-	req, err := http.NewRequest(method, url.String(), buf)
-	if err != nil {
-		return nil, err
-	}
+  req.Header.Add(client.auth.HeaderName, client.auth.ApiKey)
+  req.Header.Add("Accept", acceptedContentType)
+  req.Header.Add("Content-type", acceptedContentType)
 
-	req.Header.Add(client.auth.HeaderName, client.auth.ApiKey)
-	req.Header.Add("Accept", acceptedContentType)
-	req.Header.Add("Content-type", acceptedContentType)
-
-	return req, nil
+  return req, nil
 }
 
 // Do sends an API Request
 func (client *Client) Do(req *http.Request) ([]byte, *http.Response, error) {
 
-	response, err := client.client.Do(req)
+  response, err := client.client.Do(req)
 
 
-	if err != nil {
-		return nil, response, err
-	}
+  if err != nil {
+    return nil, response, err
+  }
 
-	if response.StatusCode >= 500 {
-		return nil, response, errors.New("Server error: " + response.Status)
-	} else if response.StatusCode >= 400 {
-		return nil, response, errors.New("Error: " + response.Status)
-	}
+  if response.StatusCode >= 500 {
+    return nil, response, errors.New("Server error: " + response.Status)
+  } else if response.StatusCode >= 400 {
+    return nil, response, errors.New("Error: " + response.Status)
+  }
 
-	defer response.Body.Close()
+  defer response.Body.Close()
 
-	data, err := ioutil.ReadAll(response.Body)
+  data, err := ioutil.ReadAll(response.Body)
 
-	if err != nil {
-		return nil, response, err
-	}
+  if err != nil {
+    return nil, response, err
+  }
 
-	return data, response, nil
+  return data, response, nil
 }
